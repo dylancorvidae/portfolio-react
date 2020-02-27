4abdbbe5ef0a3cfbfb5e7059a36220f03c3ce48a
Dylan Corvidae <50845869+dylancorvidae@users.noreply.github.com>
2020-02-26 16:11:58 -0800

    update snapshot and app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   // Switch,
-//   // Route
-// } from 'react-router-dom';
 import Header from './Header/header';
 
 export default function App() {
